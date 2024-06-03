bd8ebecd345bc5e021a61eb9bb94592c61fc1e82
Blake Whitmer <blakejwhitmer@gmail.com>
2024-06-03 20:32:38 +0200

    Initial Commit

---
+++ Playing with UI/Playing_with_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Playing_with_UIApp.swift
+//  Playing with UI
+//
+//  Created by Blake Whitmer on 6/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Playing_with_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
